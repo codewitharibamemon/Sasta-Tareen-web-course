fb259a56bb51193e98afb941314ae17f62f8117b
codewitharibamemon <codewitharibamemon@gmail.com>
2024-12-22 23:32:26 +0500

    Javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,558 @@
+// // // // //variables
+// // // // // var let const
+
+// // // // //var ES5 2015 
+
+// // // // // ES6 let const 2015
+// // // // // g;ldjg;dlldjdjg 
+// // // // /*jdfld;ljd;lj;ldjg
+// // // // dghlskhgklshg*/
+
+// // // // // var myName = "Ariba";
+// // // // // console.log(myName) // function  // errors ko check krny ky liye 
+
+
+
+
+
+// // // // // console.log("Ariba memon");
+
+
+// // // // //string,number,boolean
+
+// // // // //"", '' //string 
+
+// // // // // var a = "Ariba"; //string
+// // // // // var a = 'Ariba'; //string
+// // // // // console.log(typeof(a));
+
+// // // // // var b = 3; //number 
+// // // // // console.log(typeof(b));
+
+
+// // // // // var c = true;//boolean
+// // // // // console.log(typeof(c)) //boolean
+
+
+// // // // // let x = BigInt(9232281881888);
+// // // // // console.log(typeof(x)) //Bigint
+
+
+// // // // // var a = null;
+// // // // // console.log(a)
+
+
+
+// // // // //addition
+// // // // // var a = 2;
+// // // // // var b = 2;
+// // // // // var c = a + b ;
+// // // // // console.log(c);
+
+
+// // // // // var a = 2;
+// // // // // var b = 2;
+// // // // // var c = a - b ;
+// // // // // console.log(c);
+
+
+
+
+
+// // // // // var a = 2;
+// // // // // var b = 2;
+// // // // // var c = a * b ;
+// // // // // console.log(c);
+
+
+// // // // // var x = 5;
+// // // // // var y = 2;
+// // // // // var z = x % y;
+// // // // // console.log(z)
+
+
+
+// // // // // var myQuestion = prompt("What is your name");
+// // // // // document.write(myQuestion)
+// // // // //data types methods 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // // // var str = "Samina";
+// // // // // console.log(str.length);
+// // // // // console.log(str.toUpperCase());
+// // // // // console.log(str.toLowerCase());
+
+// // // // //concat
+
+// // // // // var str1 = "hello";
+// // // // // console.log(str1.concat("  ","world"));
+
+// // // // // var name1 = "Sadiq";
+// // // // // console.log(typeof(name1))
+
+
+// // // // // var = number(9);
+
+
+// // // // // keyword //name  //AssigningOperator  // value
+// // // //     var      id         =                   758349758934;//number
+// // // //     var      name2       =                   "fhfklfh" // string
+// // // //     var        a         =                    true  //boolean
+// // // //     // print    //variablename
+// // // //     console.log(id);
+// // // //     console.log(name2);
+// // // //     console.log(a);
+
+// // // // var myName = true;
+// // // // var myName1 = false;
+// // // // console.log(myName)
+// // // // console.log(myName1)
+// // //    // function syntax
+// // //    //keyword  functionname  parenthesis crulybraces 
+// // //     //  function   myName1(){
+// // //     //     console.log("ghjghjg");
+        
+// // //     //  }
+// // //     //  // function call 
+// // //     //  // functionname parenthesis 
+// // //     //      myName1()
+       
+
+
+
+// // // // function name1() {
+// // // //     console.log(224354);
+    
+// // // // }
+// // // // name1()
+          
+    
+
+
+
+
+
+// // // // const name3 = function () {
+// // // //  console.log("const with function");
+// // // // };
+// // // // name3();
+
+// // // //              parameter
+// // // // function greet(name){
+// // // //     console.log(name);
+    
+
+// // // // }
+// // // // greet("Ismat")
+// // // // greet("sadiq")
+
+// // // // function add(a,b){
+// // // //     return a+b
+// // // // }
+// // // // console.log(add(2,6));
+
+
+// // // // Array         index       0       ,   1       ,     2
+
+// // // // var studentsName = ["Sadiq Khan" ,"Ismat Zehra","aliza fatima", 64893653,"true"];
+// // // // //console.log(studentsName[2]);
+// // // // console.log(studentsName);
+ 
+
+// // // let studentsName = new Array("Sadiq Khan" ,"Ismat Zehra","aliza fatima", 64893653,"true");
+
+// // // console.log(studentsName);
+
+
+
+
+
+
+
+
+
+
+// //  //objects
+// // let student = {
+// //     name :"Anabia",
+// //     age : 9,
+// //     id : 3479274,
+// //     email : "anabia@gmail.com",
+
+
+// // }
+// // // console.log(student);
+// // console.log(student.email);
+
+// // let Car = {
+// //     name : "Flat",
+// //     model: 500,
+// //     weight : "850kg",
+// //     color : "white"
+// // }
+// // console.log(Car)
+// // const person = new Object(
+    
+// // );
+
+// // person.firstName = "Ariba"
+// // person.lastName = "Memon"
+// //Increment Decrement
+// //Postfix, Prefix
+// //++,--
+// // let x = 50;
+// // x++; //Postfix baad main plus plus arha hai
+// // ++x; //prefix
+// // console.log(x);
+// // let y = 10;
+// // y--; //
+// // console.log(y);
+
+
+// // var x = 5;
+// // var y = 6;
+// // var z = x++ + ++x + --y + y-- - --x ;
+// //      // 5+1 + 7 +    5  + 
+// //      // 5   + 
+// //       // 6  +   5  +   5  +   5   -  4
+// //       // 17
+// // console.log(z);
+
+
+
+
+
+// // var x = 5;
+// // var y = 6;
+// // var z = x++ + ++y;
+// //       // 5   +  7 // 12
+// // console.log(z)
+
+
+
+
+// // var t = 2;
+// // var w = 4;
+// // var f = t++ + ++w + --t + --w ;
+// // //      2   +  5  +  1  + 3  
+// //    //   2+1 =3 
+// //         // 2 + 5  + 2 + 4 ; // 
+// //          //13 
+// // console.log(f);
+
+
+        
+
+
+
+// //Comparison Operators
+// // let x = 5;
+// // let y = 5;
+// // console.log(x==y);
+// // let x = 5;
+// // let y = '5';
+// //  console.log(x==y);
+// // let x = 5;
+// // let y = '5';
+// //  console.log(x===y);
+// // let x = 6;
+// // let y = 8;
+// // console.log(x != y);
+// // let x = 6;
+// // let y = 6;
+// // console.log(x != y);
+// // let x = 6;
+// // let y = '8';
+// // console.log(x !== y);
+// // let x = 9;
+// // let y = 8;
+// // console.log(x > y);
+// //< less than
+// // let x = 9;
+// // let y = 8;
+// // console.log(x < y);
+// // let x = 9;
+// // let y = 8;
+// // console.log(x <= y);
+// // let x = 9;
+// // let y = 8;
+// // console.log(x >= y);
+// // if else
+// // let a = 5;
+// // let b = 6;
+// // if (a > b) {
+// //     console.log("a is greater than b");
+    
+// // } else {
+// //     console.log("a is not greater than b");
+    
+// // }
+
+
+
+
+
+
+
+
+
+
+
+// // function name(params) {
+    
+// // }
+
+
+
+
+
+
+// // let minhal = {
+// //     firstname : "Minhal",
+// //     lastname: "Nasir"   ,
+// //     age : "18",
+// //     edu :"CS student"
+// //    }
+// //  console.log(minhal)
+
+
+
+// //  const firstString = "Hello";
+// // const secondString = "World";
+
+// // const result = firstString.concat(" ", secondString);
+
+// // console.log(result);
+
+
+
+// // const variable = 10 
+// //     If ( variable > a){
+// //     console.log("variable 10 Sy bara hn")
+// //     }else {
+// //     console.log("variable a sy chota hn")
+// //     }
+
+// // var num = 10;
+// // if(num > 10){
+// //     console.log("Allow")
+// // }
+// // else{
+// //     console.log("Not allow")
+// // }
+
+
+// // let number = BigInt(5425686362836);
+// // console.log(number);
+
+// // let num = 65578836836;
+// // console.log(BigInt(num));
+
+
+// // var x =2;
+// // var z =x++ + x--;
+// // console.log(z)
+
+
+// // var a = "Ismat";                                                  
+// //  console.log(a)
+
+
+
+
+
+
+// // function addTwoNumbers(num1, num2) {
+// //     let sum = num1+ num2;
+// //     console.log(sum);
+
+// //     }
+// //     addTwoNumbers(2,3)
+
+
+
+
+
+
+// //loops
+
+
+// // console.log(1);
+// // console.log(2);
+// // console.log(3);
+// // console.log(4);
+// // console.log(5);
+// //         intitializtion,  condition ,  increment/decremen
+// // for (var i = 1 ; i <= 500 ; i++){
+// //     console.log(i);
+    
+// // }
+
+
+
+// // var students = ['Aisha','Sadiq','Maryam'];
+// // // index hamesha 0 sy shuru hotahai 
+// // // console.log(students.length);
+// // // console.log(students.shift());
+// // // console.log(students)
+// // // console.log(students.unshift("Aisha"));
+// // // console.log(students)
+
+// // console.log(students.pop());
+// // console.log(students)
+// // console.log(students.push('Ismat'));
+// // console.log(students)
+// // //lenght hamesha 1 sy start hote hai
+
+
+
+
+// // //ES5 
+
+// // var firstName = "Ariba";
+// // var lastName = " Memon";
+// // console.log(firstName + lastName);
+
+
+// // //ES6 ,ES7
+// // var firstName = "Ariba";
+// // var lastName = "Memon";
+// // console.log(`${firstName} ${lastName}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // for (var i = 1 ; i <= 500 ; i++){
+// //     console.log(i);
+
+// //for of 
+// // const arrays = ["Rizwan", "Ishmat","Abdullah"];
+// // // console.log(arrays);
+// // const arrays = ["Rizwan", "Ishmat","Abdullah"];
+// // for(let array of arrays){
+// // console.log(array);
+
+
+// // }
+// // for(let index in arrays){
+// // console.log(index, arrays[index]);
+
+// // }
+// //for in
+
+// // const person ={
+// //     name : "sdjkal",
+// //     age : 24,
+// //     city :"Karachi"
+// // }
+// // for(let key in person){
+// //     console.log(`${key}: ${person[key]}`);
+    
+
+// // }
+// // Difference between var, const , let
+// // var myName = "Samina";
+// // var myName = "Ariba";
+// // console.log(myName);
+// // let myName = "Samina";
+// // myName ="Ariba"
+// // // let myName = "Ariba";
+// // console.log(myName);
+// // const myName = "Samina";
+// // // const  myName = "Ariba";
+// // myName ="Ariba";
+// // console.log(myName);
+// // const name = prompt('what is your name');
+// // console.log(name);
+
+// // const num1 = prompt("enter first no");
+// // const operators = prompt("Add operator");
+// // const num2 = prompt("enter second no");
+
+// // let result;
+
+// // if(operators === "+"){
+// //     result = num1 + num2;
+// // }else if (operators === "-"){
+// //     result = num1 - num2;
+// // }else if(operators === "*"){
+// //     result = num1 * num2;
+// // }else{
+// //     result = "wrong option"
+// // }
+// // alert(result)
+
+
+// // arrow function
+// // function greet(name){
+// //     return("hello"+ name)
+// // }
+// // console.log(greet("Ariba"));
+
+// // const greet =(name) => {
+// //     return "hello"+ name
+// // }
+
+// // console.log(greet("Ariba"));
+
+// const greet1 =(name1) => "hello" + name1
+// console.log(greet1("Anabia"));
+
+
+
+// // for (var i = 1 ; i <= 500 ; i++){
+// //     console.log(i);
+
+// for in 
+// let Student ={
+//     name: "Abdullah",
+//     age : 19,
+//     id : 46892242
+// }
+// for(let key in Student){
+//     console.log(`${key} : ${Student[key]}`);
+    
+// }
+// let colors =["red", "blue","orange"];
+//  console.log(colors);
+// for(let color of colors){
+//     console.log(color);
+    
+// }
+// for(let key in colors){
+//     console.log(`${key}: ${colors[key]}`);
+    
+}
+
+
+
